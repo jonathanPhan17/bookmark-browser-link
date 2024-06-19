be117f65699be10449d43bd4704afdeda0ec92d3
Jonathan Phanthawong <164473372+jonathanPhan17@users.noreply.github.com>
2024-06-18 23:46:54 -0700

    create controlled components

--- src/Components/ParkingLotForm/ParkingLotForm.jsx
+++ src/Components/ParkingLotForm/ParkingLotForm.jsx
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, FormGroup, Label, Input } from 'reactstrap'; 
 
 import './ParkingLotForm.css'
 
+  const PRIORITIES = {
+    Low: "Low",
+    Medium: "Medium",
+    High: "High",
+  };
+
 export default function ParkingLotForm() {
+
+  const [date, setDate] = useState("");
+  const [link, setLink] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState(PRIORITIES.Medium);
+
+  function handleDateChange(e) {
+    setDate(e.target.value);
+  }
+
+  function handleLinkChange(e) {
+    setLink(e.target.value);
+  }
+
+  function handleDescriptionChange(e) {
+    setDescription(e.target.value);
+  }
+
+  function handlePriorityChange(e) {
+    setPriority(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(date, link, description, priority)
+  }
+
     return (
-      <Form data-bs-theme="dark" className="parking-lot-form">
+      <Form data-bs-theme="dark" className="parking-lot-form" onSubmit={handleSubmit}>
         <FormGroup className="parking-lot-row">
           <Label htmlFor="link-date">Date</Label>
-          <Input id="link-date" name="date" type="date" required />
+          <Input
+            id="link-date"
+            name="date"
+            type="date"
+            value={date}
+            onChange={handleDateChange}
+            required
+          />
         </FormGroup>
         <FormGroup className="parking-lot-row">
           <Label htmlFor="link-url">Link</Label>
-          <Input id="link-url" name="url" type="url" required />
+          <Input
+            id="link-url"
+            name="url"
+            type="url"
+            value={link}
+            onChange={handleLinkChange}
+            required
+          />
         </FormGroup>
         <FormGroup className="parking-lot-row">
           <Label htmlFor="link-description">Description</Label>
@@ -20,6 +67,8 @@ export default function ParkingLotForm() {
             id="link-description"
             name="description"
             type="text"
+            value={description}
+            onChange={handleDescriptionChange}
             required
           />
         </FormGroup>
@@ -27,7 +76,9 @@ export default function ParkingLotForm() {
           <Input
             name="radio-priority"
             type="radio"
-            value="High"
+            value={PRIORITIES.High}
+            checked={priority === PRIORITIES.High}
+            onChange={handlePriorityChange}
             id="prio-high"
           />
           &nbsp;
@@ -38,7 +89,9 @@ export default function ParkingLotForm() {
           <Input
             name="radio-priority"
             type="radio"
-            value="Medium"
+            value={PRIORITIES.Medium}
+            checked={priority === PRIORITIES.Medium}
+            onChange={handlePriorityChange}
             id="prio-medium"
           />
           &nbsp;
@@ -46,7 +99,14 @@ export default function ParkingLotForm() {
             Medium
           </label>
           &nbsp;
-          <Input name="radio-priority" type="radio" value="Low" id="prio-low" />
+          <Input
+            name="radio-priority"
+            type="radio"
+            value={PRIORITIES.Low}
+            checked={priority === PRIORITIES.Low}
+            onChange={handlePriorityChange} 
+            id="prio-low"
+          />
           &nbsp;
           <label htmlFor="prio-low" className="radio-btn-row">
             Low
